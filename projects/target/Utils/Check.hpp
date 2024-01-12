f272f80e4a3664425857e16a1c9adff1652a694d
nekosu <liao.hy@outlook.com>
2024-01-12 20:23:59 +0800

    feat: add missing float point schema

--- projects/target/Utils/Check.hpp
+++ projects/target/Utils/Check.hpp
@@ -15,7 +15,7 @@ struct check_t
 template <typename Type>
 struct schema_t
 {
-    static constexpr const char* const schema = "unknown";
+    static constexpr const char* const schema = "{}";
 };
 
 template <typename Type>
@@ -29,7 +29,7 @@ bool check_var(json::value v) = delete;
 template <>
 struct schema_t<void>
 {
-    static constexpr const char* const schema = "null";
+    static constexpr const char* const schema = "{ \"type\": \"null\" }";
 };
 
 #pragma endregion
@@ -45,7 +45,7 @@ struct check_t<bool>
 template <>
 struct schema_t<bool>
 {
-    static constexpr const char* const schema = "boolean";
+    static constexpr const char* const schema = "{ \"type\": \"boolean\" }";
 };
 
 template <>
@@ -67,11 +67,36 @@ struct check_t<Type>
 template <std::integral Type>
 struct schema_t<Type>
 {
-    static constexpr const char* const schema = "number";
+    static constexpr const char* const schema = "{ \"type\": \"number\" }";
 };
 
 template <std::integral Type>
 requires (!std::is_same_v<Type, bool>)
+bool check_var(json::value v)
+{
+    return v.is_number();
+}
+
+#pragma endregion
+
+#pragma region floating
+
+template <typename Type>
+concept floating_point = std::is_floating_point_v<Type>;
+
+template <floating_point Type>
+struct check_t<Type>
+{
+    static constexpr const bool enable = true;
+};
+
+template <floating_point Type>
+struct schema_t<Type>
+{
+    static constexpr const char* const schema = "{ \"type\": \"number\" }";
+};
+
+template <floating_point Type>
 bool check_var(json::value v)
 {
     return v.is_number();
@@ -90,7 +115,7 @@ struct check_t<const char*>
 template <>
 struct schema_t<const char*>
 {
-    static constexpr const char* const schema = "string";
+    static constexpr const char* const schema = "{ \"type\": \"string\" }";
 };
 
 template <>
