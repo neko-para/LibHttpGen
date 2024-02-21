1945dd19202f1635f2e24ecc9a6acbd477c88a0e
nekosu <liao.hy@outlook.com>
2024-02-21 11:14:17 +0800

    chore: use std::format

--- projects/target/Helper/Format.hpp
+++ projects/target/Helper/Format.hpp
@@ -1,22 +1,8 @@
 #pragma once
 
-#ifdef LHG_USE_FMTLIB
-
-#define FMT_HEADER_ONLY
-#include <fmt/format.h>
-
-namespace FMT
-{
-using fmt::format;
-}
-
-#else
-
 #include <format>
 
 namespace FMT
 {
 using std::format;
 }
-
-#endif
